8e5860b637e6fea4f7859e586a9202f72b283ed4
Lee Inmok <tomato.202404.leeim@gmail.com>
2024-06-27 16:58:27 +0900

    client design

--- src/page/clientpage/Client.tsx
+++ src/page/clientpage/Client.tsx
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from "react";
 import { useLocation } from "react-router-dom";
 import { fetchUserAttributes } from "aws-amplify/auth";
+import { Button } from "@aws-amplify/ui-react";
+import axios from "axios";
 
 interface userdata {
   birthdate: string | undefined;
@@ -43,6 +45,27 @@ const Client: React.FC = () => {
     console.log(userAttributes);
   }, [userAttributes]);
 
+  const workstart = async () => {
+    try {
+      const response = await axios.post(
+        `https://jlfxm6r10g.execute-api.us-east-1.amazonaws.com/dev/worker?QRUUID=018f4bea-5271-4dbb-a1e7-1a148c921543&name=LEE INMOK&hhmmss=145300&yymmdd=240627&state=1`
+      );
+
+      console.log("Post request successful", response.data);
+    } catch (error) {
+      console.error("Error while making POST request:", error);
+    }
+  };
+  const workoff = () => {
+    console.log("退勤");
+  };
+  const goout = () => {
+    console.log("外出");
+  };
+  const returnwork = () => {
+    console.log("復帰");
+  };
+
   return (
     <div>
       <h1>QR Data</h1>
@@ -68,6 +91,10 @@ const Client: React.FC = () => {
       ) : (
         <p>Loading user attributes...</p>
       )}
+      <Button onClick={workstart}>出勤</Button>
+      <Button onClick={workoff}>退勤</Button>
+      <Button onClick={goout}>外出</Button>
+      <Button onClick={returnwork}>復帰</Button>
     </div>
   );
 };
